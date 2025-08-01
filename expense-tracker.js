14b57850ee3eae147eeec3316951921074a642b2
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-08-01 22:08:30 +0600

    fixing  generateReport

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,7 +1,7 @@
-let expenses = [];
-let nextId = 1;
+let expenses = []; //array to store expenses
+let nextId = 1; //next id for new expenses
 
-function addExpenses(amount, category){
+function addExpenses(amount, category){ //function to add expenses
     const newExpense  = { 
         id: nextId++,
         amount: amount,
@@ -10,18 +10,18 @@ function addExpenses(amount, category){
     expenses.push(newExpense);
     console.log(`Added #${newExpense.id}: $${amount} (${category})`);
 }
-function viewExpenses(){
+function viewExpenses(){ //function to view expenses
     for(let i = 0; i<expenses.length; i++){
         console.log(`#${expenses[i].id} | $${expenses[i].amount} | ${expenses[i].category.toUpperCase()}`);
 }}
-function ViewByCategory(category){
+function ViewByCategory(category){ //function to view expenses by category
 for(let i = 0 ; i<expenses.length; i++){
     if(expenses[i].category === category){
         console.log(`#${expenses[i].id} | $${expenses[i].amount} | ${expenses[i].category.toUpperCase()}`);
     }
 }
 }
-function CalculateTotal(){
+function CalculateTotal(){ //function to calculate total expenses
     let  total = 0;
     for(let i = 0; i<expenses.length; i++){
         total += expenses[i].amount;
@@ -29,7 +29,7 @@ function CalculateTotal(){
     return total;
     // console.log(`Total: $${total}`);
 }
-function RemoveExpense(id){
+function RemoveExpense(id){ //function to remove expenses
 for(let i = 0; i<expenses.length; i++){
     if(expenses[i].id === id){
         expenses.splice(i,1)
@@ -40,7 +40,7 @@ viewExpenses();
 }
 console.log(`Expense #${id} not found`);
 }
-function GenerateReport(){
+function GenerateReport(){ //function to generate report
     console.log("Expense Report");
     console.log("==================");
     viewExpenses();
@@ -48,9 +48,8 @@ function GenerateReport(){
     console.log("==================");
     console.log("Expense Report");
     console.log("==================");
-    viewExpenses();
 }
-function showMenu(){
+function showMenu(){ //function to show menus
     console.log(`1. addExpenses(amount, category)
         2. viewExpenses()
         3. ViewByCategory(category)
